753629b3b3f083423b5c5fec12eceb4276450685
Vladimir <smile_friend@mail.ru>
2023-05-07 23:10:07 +0300

    final class

--- WheatherApp/ViewControllers/WheatherTableViewController.swift
+++ WheatherApp/ViewControllers/WheatherTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WheatherTableViewController: UITableViewController {
+final class WheatherTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
